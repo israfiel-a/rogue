827c57be428075d404843b34eaa5680c76905f92
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-06-26 12:31:10 -0400

    updated for new init args

--- Source/Example.c
+++ Source/Example.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
     (void)argc;
     (void)argv;
 
-    bool success = waterlily_initialize("Example");
+    bool success = waterlily_initialize("Example", 0);
     if (!success) return -1;
 
     waterlily_run();
