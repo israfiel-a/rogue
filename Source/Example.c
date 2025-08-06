5e9574478a7048c354f87e4597a0bcbe3bb385e4
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-08-06 01:24:50 -0400

    updated for new engien

--- Source/Example.c
+++ Source/Example.c
@@ -2,10 +2,13 @@
 
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
+    char *argvExpanded[argc + 2];
+    for(size_t i = 0; i < (size_t)argc; ++i)
+        argvExpanded[i] = argv[i];
+    argvExpanded[argc] = "Example";
+    argvExpanded[argc + 1] = "0";
 
-    bool success = waterlily_initialize("Example", 0);
+    bool success = waterlily_initialize(argc + 2, argvExpanded);
     if (!success) return -1;
 
     waterlily_run();
