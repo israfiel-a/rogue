f8e1d4817063baf9acba7d9cf5891221b5824c48
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-08-06 03:16:59 -0400

    oops

--- Source/Entry.c
+++ Source/Entry.c
@@ -3,7 +3,7 @@
 int main(int argc, const char *const *const argv)
 {
     waterlily_arguments_t arguments = {0};
-    waterlily_engine_digest(argc, argc);
+    waterlily_engine_digest(argc, argv);
 
     return 0;
 }
