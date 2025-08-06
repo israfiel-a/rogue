62d8eea4c1d3c3a65084aebd10af3e837475890b
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-08-06 12:51:28 -0400

    added vulkan init code

--- Source/Entry.c
+++ Source/Entry.c
@@ -1,17 +1,23 @@
 #include <Waterlily.h>
 
-int main(int argc, const char *const *const argv)
-{
+int main(int argc, const char *const *const argv) {
     waterlily_arguments_t arguments = {0};
     waterlily_engine_digest(argc, argv, &arguments);
 
     waterlily_engine_setup(&arguments);
 
     waterlily_window_create("Rogue");
-    while(waterlily_window_process())
-    {
+
+    VkInstance instance = nullptr;
+    if (!waterlily_vulkan_create(&instance)) return -1;
+    VkSurfaceKHR surface = nullptr;
+    if (!waterlily_vulkan_link(instance, &surface)) return -1;
+
+    while (waterlily_window_process()) {
         __asm("");
     }
+
+    waterlily_vulkan_destroy(instance);
     waterlily_window_destroy();
 
     return 0;
