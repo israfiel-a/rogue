fc3428fe0bf64b1afd2bab8f3ce02171718e5271
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-08-07 02:50:13 -0400

    more updates to the structure, added custom waterlily dir

--- Source/Entry.c
+++ Source/Entry.c
@@ -38,6 +38,33 @@ int main(int argc, const char *const *const argv)
                                            &indices))
         return -1;
 
+    VkSwapchainKHR swapchain = nullptr;
+    uint32_t imageCount = 0;
+    if (!waterlily_vulkan_createSwapchain(logical, &imageCount, &swapchain,
+                                          &surface, &indices))
+        return -1;
+
+    VkImageView images[imageCount];
+    if (!waterlily_vulkan_partitionSwapchain(logical, &surface, swapchain,
+                                             imageCount, images))
+        return -1;
+
+    waterlily_vulkan_graphics_pipeline_t pipeline = {0};
+
+    VkPipelineShaderStageCreateInfo stages[2];
+    const char *const stageNames[2] = {"default.vert", "default.frag"};
+    if (!waterlily_vulkan_setupShadersPipeline(logical, stageNames, 2, stages))
+        return -1;
+
+    waterlily_vulkan_pipeline_info_t info = {0};
+    waterlily_vulkan_fillInfoPipeline(&surface, &info);
+
+    if (!waterlily_vulkan_createLayoutPipeline(logical, &pipeline) ||
+        !waterlily_vulkan_createRenderpassPipeline(logical, &pipeline,
+                                                   &surface) ||
+        !waterlily_vulkan_createPipeline(logical, &pipeline, stages, 2, &info))
+        return -1;
+
     while (waterlily_window_process())
     {
         __asm("");
